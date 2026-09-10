9e87c3e69bd9ee5f2ac9a2a5fec054cd1c31a4c7
Rio Hutagalung <rio.hutagalung2@gmail.com>
2026-09-10 18:04:04 +0700

    fix Capacitor webDir for native builds

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.rhhabits.app',
   appName: 'RH Habits',
-  webDir: '.',
+  webDir: 'www',
   android: {
     useLegacyBridge: true
   },
